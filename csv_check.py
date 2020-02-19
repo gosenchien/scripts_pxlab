55adc13f777c45ebcd1931864003f96500ed6924
Nova1987a <32932204+gosenchien@users.noreply.github.com>
2020-02-19 15:39:11 +0800

    new files

---
+++ csv_check.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#
+#   A script for comparing different column in a sheet
+#
+#
+
+import csv
+list1 = []
+list2 = []
+dupli = []
+
+with open('csv_comp.csv', newline='') as csvfile:
+    rows = csv.reader(csvfile)
+    for row in rows:
+        list1.append(row[0].lower())
+        list2.append(row[2].lower())
+    ## Test duplication
+#    for j in list2:
+#        if list2.count(j)>1:
+#            dupli.append(j)
+#    print(dupli)
+    for i in list(set(list1) & set(list2)):
+        print(i)
