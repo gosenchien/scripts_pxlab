29f8290d49a214989084d582f8ea47e9d6e783ae
Nova1987a <32932204+gosenchien@users.noreply.github.com>
2020-05-15 11:41:58 +0800

    Update sshexpect.sh
    Add foreach function

--- sshexpect.sh
+++ sshexpect.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/expect
 
-spawn ssh -o StrictHostKeyChecking=no adb@100.91.160.190 
-expect "ssword:" { send "password\r" }
-expect "$ " { send "echo 'password'|sudo -S apt-get update\r" }
-interact
 
+expect "$ " set lst [ list 17 18 19 ]
+
+foreach i $lst {
+	spawn ssh -o StrictHostKeyChecking=no pixellab@100.91.160.$i
+	expect "ssword:" { send "PMLJJ@1212\r" }
+	expect "$ "	{ send "echo 'PMLJJ@1212'|sudo -S apt-get update\r" }
+	expect "$ "	{ send "exit" }
+	
+}
